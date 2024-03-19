05e1e51cd5b63003f91e57bfd7149729868a7f3d
hwsidec <hws@idec.ch>
2024-03-19 15:50:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btnWelcome").addEventListener("click", function() {
+    document.getElementById("welcomeMessage").style.display = "block";
+    document.getElementById("welcomeText").innerText = "Bienvenue sur notre site web !";
+});
